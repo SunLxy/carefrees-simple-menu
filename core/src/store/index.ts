ea44d93d6eb027ad8dc2315e2bbdcdcfe01e32ab
SunLxy <1011771396@qq.com>
2023-08-04 07:34:48 +0800

    fix:优化代码

--- core/src/store/index.ts
+++ core/src/store/index.ts
@@ -3,14 +3,12 @@ import { useRef } from "react"
 export class Store {
   /**选中值*/
   private value: string | undefined = undefined
-  private prevPath: string | undefined
 
   /**对应的方法调用*/
   private componentMap: Map<string, Function> = new Map([])
 
   init(value: string | undefined) {
     this.value = value
-    this.prevPath = value
   }
 
   /**把对应更新单个组件的方法进行存储*/
@@ -25,12 +23,12 @@ export class Store {
 
   /**更新值*/
   updateValue(value: string | undefined) {
+    const prevPath = this.value
     this.value = value
     /**把上一次的组件进行更新*/
-    this.notice(this.prevPath)
+    this.notice(prevPath)
     /**更新当前选中组件*/
     this.notice(value)
-    this.prevPath = value
   }
 
   /**通知对应组件更新*/
